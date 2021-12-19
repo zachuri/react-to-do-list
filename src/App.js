5300713fc085a6d61cb4f7f6b1dbbf90deae79a9
zachuri <punsalang.zachary@gmail.com>
2021-12-18 17:25:10 -0800

    cache

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
 import React, { Component } from "react";
 import "./App.css";
-import Counter2 from "./components/Counter2";
-import ImageSlider from "./components/ImageSlider";
-// import TodoList from "./components/TodoList";
+// import FetchRandomUser from "./components/FetchRandomUser";
+// import Counter2 from "./components/Counter2";
+// import ImageSlider from "./components/ImageSlider";
+// import MyForm from "./components/MyForm";
+// import ValidationForm from "./components/ValidationForm";
+import TodoList from "./components/TodoList";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			visible: true,
-			whichComponentToShow: "ImageSlider",
-		};
-	}
-
 	render() {
-		if (this.state.whichComponentToShow === "ImageSlider") {
-			return (
-				<div className="App">
-					<ImageSlider />
-					<button
-						onClick={() => {
-							this.setState({ whichComponentToShow: "Counter2" });
-						}}
-					>
-						Show Counter
-					</button>
-				</div>
-			);
-		} else if (this.state.whichComponentToShow === "Counter2") {
-			return (
-				<div className="App">
-					<Counter2 />
-					<button
-						onClick={() => {
-							this.setState({ whichComponentToShow: "ImageSlider" });
-						}}
-					>
-						Show Image Slider
-					</button>
-				</div>
-			);
-		} else {
-			return null;
-		}
+		return (
+			<div className="App">
+				<TodoList />
+			</div>
+		);
+
+		// if (this.state.whichComponentToShow === "ImageSlider") {
+		// 	return (
+		// 		<div className="App">
+		// 			<ImageSlider />
+		// 			<button
+		// 				onClick={() => {
+		// 					this.setState({ whichComponentToShow: "Counter2" });
+		// 				}}
+		// 			>
+		// 				Show Counter
+		// 			</button>
+		// 		</div>
+		// 	);
+		// } else if (this.state.whichComponentToShow === "Counter2") {
+		// 	return (
+		// 		<div className="App">
+		// 			<Counter2 />
+		// 			<button
+		// 				onClick={() => {
+		// 					this.setState({ whichComponentToShow: "ImageSlider" });
+		// 				}}
+		// 			>
+		// 				Show Image Slider
+		// 			</button>
+		// 		</div>
+		// 	);
+		// } else {
+		// 	return null;
+		// }
 	}
 }
 
